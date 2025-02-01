ee14128e427b36cd03643f4977b1305634a97121
TobiasRW <tobiasrw98@gmail.com>
2025-02-01 16:59:35 +0100

    color tailwind setup + image setup i next config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        blue: "var(--blue)",
+        blueSec: "var(--blueSec)",
         card: {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",

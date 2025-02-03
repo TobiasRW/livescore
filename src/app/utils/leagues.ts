6e5ba6db9830265a48f3c14af6afdb404e2b3a42
TobiasRW <tobiasrw98@gmail.com>
2025-02-03 19:14:53 +0100

    map league ids to string values from second api

---
+++ src/app/utils/leagues.ts
@@ -0,0 +1,7 @@
+export const leagueIdToCode: Record<number, string> = {
+  39: "PL", // Premier League (England)
+  140: "PD", // La Liga (Spain)
+  78: "BL1", // Bundesliga (Germany)
+  61: "FL1", // Ligue 1 (France)
+  135: "SA", // Serie A (Italy)
+};

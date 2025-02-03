243125f6939c888a0a359bd2deb4f843ee410165
TobiasRW <tobiasrw98@gmail.com>
2025-02-03 19:14:10 +0100

    allow images from second api + tailwind screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,15 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "400px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1792px",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",

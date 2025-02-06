441fca3c2ffde4b9da32360398564a0bb20eddd1
TobiasRW <tobiasrw98@gmail.com>
2025-02-06 16:33:39 +0100

    added demo match link on front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Match } from "@/types/match";
 import { getLiveScores } from "../actions/live.actions";
 import MatchCard from "@/components/match-card";
 import Image from "next/image";
+import Link from "next/link";
 
 export const revalidate = 60; // // revalidate the page after 60 seconds
 
@@ -35,29 +36,44 @@ export default async function Home() {
           ))
         ) : (
           // If no live matches are available, display this message
-          <div className="flex flex-col items-center gap-4 rounded-lg bg-[#f0efef] py-6 drop-shadow-xl dark:bg-[#202020]">
-            <p className="mx-auto w-11/12 text-center font-body text-sm font-semibold italic text-black sm:text-base dark:text-white">
-              No live matches currently available at this time 😢
-            </p>
-            <div className="mx-auto w-11/12 font-body text-sm font-medium italic text-black sm:text-base dark:text-white">
-              <p> This app shows live scores from the following leagues:</p>
-              <ul className="mt-2 flex flex-col gap-1 font-light">
-                <li>1. UEFA Champions League 🇪🇺</li>
-                <li>2. UEFA Europa League 🇪🇺</li>
-                <li>3. UEFA Conference League 🇪🇺</li>
-                <li>4. FIFA World Cup 🇪🇺</li>
-                <li>5. European Championship 🇪🇺</li>
-                <li>6. Dansih Superliga 🇩🇰</li>
-                <li>7. English Premier League 🏴󠁧󠁢󠁥󠁮󠁧󠁿</li>
-                <li>8. English Championship 🏴󠁧󠁢󠁥󠁮󠁧󠁿</li>
-                <li>9. Spanish La Liga 🇪🇸</li>
-                <li>10. Italian Serie A 🇮🇹</li>
-                <li>11. German Bundesliga 🇩🇪</li>
-                <li>12. French Ligue 1 🇫🇷</li>
-                <li>13. Portuguese Primeira Liga 🇵🇹</li>
-              </ul>
-              <br />
-              <p>Come back later to check for live scores and stats! 🕒</p>
+
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col items-center gap-4 rounded-lg bg-[#f0efef] py-6 drop-shadow-xl dark:bg-[#202020]">
+              <p className="mx-auto w-11/12 text-center font-body text-sm font-semibold italic text-black sm:text-base dark:text-white">
+                No live matches currently available at this time 😢
+              </p>
+              <p className="mx-auto w-11/12 text-center font-body text-sm font-light italic text-black dark:text-white">
+                To see a demo of the app i action, you can see stats from a
+                previous match.
+              </p>
+              <Link
+                href="/match/1213921"
+                className="flex items-center rounded-lg bg-[#1D3461] px-4 py-2 font-heading text-sm text-white"
+              >
+                View Demo Match
+              </Link>
+            </div>
+            <div className="flex flex-col items-center gap-4 rounded-lg bg-[#f0efef] py-6 drop-shadow-xl dark:bg-[#202020]">
+              <div className="mx-auto w-11/12 font-body text-sm font-medium italic text-black sm:text-base dark:text-white">
+                <p> This app shows live scores from the following leagues:</p>
+                <ul className="mt-2 flex flex-col gap-1 font-light">
+                  <li>1. UEFA Champions League 🇪🇺</li>
+                  <li>2. UEFA Europa League 🇪🇺</li>
+                  <li>3. UEFA Conference League 🇪🇺</li>
+                  <li>4. FIFA World Cup 🇪🇺</li>
+                  <li>5. European Championship 🇪🇺</li>
+                  <li>6. Dansih Superliga 🇩🇰</li>
+                  <li>7. English Premier League 🏴󠁧󠁢󠁥󠁮󠁧󠁿</li>
+                  <li>8. English Championship 🏴󠁧󠁢󠁥󠁮󠁧󠁿</li>
+                  <li>9. Spanish La Liga 🇪🇸</li>
+                  <li>10. Italian Serie A 🇮🇹</li>
+                  <li>11. German Bundesliga 🇩🇪</li>
+                  <li>12. French Ligue 1 🇫🇷</li>
+                  <li>13. Portuguese Primeira Liga 🇵🇹</li>
+                </ul>
+                <br />
+                <p>Come back later to check for live scores and stats! 🕒</p>
+              </div>
             </div>
           </div>
         )}
